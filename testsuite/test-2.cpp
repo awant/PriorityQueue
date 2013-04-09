47d61054a3fe74c28f17b76e62af01168ef44344
awant <mir0810@mail.ru>
2013-04-09 08:33:29 -0300

    Update test-2.cpp

--- testsuite/test-2.cpp
+++ testsuite/test-2.cpp
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include "iostream"
 #include "PriorQueue.h"
-#include <conio.h>
 
 int main()
 {
@@ -16,6 +15,5 @@ int main()
 	extr = QP.Extract();
 	std::cout<<QP;
 	printf("extract value = %d", extr);
-	_getch();
 	return (0);
 }
